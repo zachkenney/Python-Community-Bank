ebf98ea96996a208e7c5c50df6b2d4c6a7063355
zachkenney <zachkenney.zk@gmail.com>
2026-04-29 19:17:49 -0400

    Setting up link to DB

--- bank.py
+++ bank.py
@@ -1,3 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+
 class User:
     # Class for creating/getting user
     pass
